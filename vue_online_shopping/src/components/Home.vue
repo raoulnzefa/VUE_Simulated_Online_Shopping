92688deed60f40e93bfd81365a6bbb284a0a9c2c
Jason Qu <fishinsea2014@gmail.com>
2018-07-17 22:49:14 +1200

    Setup project

---
+++ vue_online_shopping/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <banner/>
+       <list/>
+    </div>
+</template>
+
+<script>
+// import banner from './banner'
+// import list from './list'
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    components: {
+        // list,
+        // banner
+    }
+}
+</script>
---
+++ vue_online_shopping/src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+import routes from './router.config'
+import axios from  'axios'
+import VueLazyload from 'vue-lazyload'
+import './assets/font/iconfont.css'
+import './assets/css/base.css'
+import VueRouter from '../node_modules/vue-router';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes
+})
+
+
+Vue.config.productionTip = false  //Stop production tips
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  //1.0 style
+  // components: { App },
+  // template: '<App/>'
+  render: h => h(App) //2.0 style, means create an app
+})
