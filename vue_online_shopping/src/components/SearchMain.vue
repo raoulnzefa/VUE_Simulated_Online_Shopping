08d3058866376818b890879f93635ba79941c846
Jason Qu <fishinsea2014@gmail.com>
2018-07-19 11:57:51 +1200

    Complete search page.

--- vue_online_shopping/src/components/SearchMain.vue
+++ vue_online_shopping/src/components/SearchMain.vue
@@ -10,6 +10,53 @@
                     </form>
                     <a href="#" class="icon_menu"></a>
                 </header>
+                <section class='search_condition'>
+                    <ul>
+                        <li>
+                            <span class="all">ALL</span>
+                            <em class="all_icon"></em>
+                        </li>
+                        <li>
+                            <span @click="goSearch('hot')">Volume</span>
+                        </li>
+                        <li class="p_price">
+                                <span>Price</span>
+                                <em class="price_up" @click="getByPriceUp()"></em>
+                                <em class="price_down" @click="getByPriceDown()"></em>
+                            </li>
+                            <li>
+                                <span>Filter</span>
+                                <em class="saixuan"></em>
+                            </li>
+                    </ul>
+                </section>
+                <main class="main_goods_box">
+                    <ul>
+                        <li class="goods_item" v-for="item in mDatas">
+                            <router-link :to="'/detail/'+item.product_id" class="goods_item_link">
+                                <img v-lazy="item.product_img_url" alt="" class="goods_item_pic">
+                                <div class="goods_right">
+                                    <div class="pp_name">
+                                        <span>
+                                            {{item.product_name}}
+                                        </span>
+                                    </div>
+                                    <div class="price_box">
+                                            <span>$</span>
+                                            <span>{{item.product_uprice}}</span>
+                                            <span>.00</span>
+                                        </div>
+                                        <div class="pinglun_box">
+                                            <span>{{item.product_comment_num}} comments</span>
+                                            <span>{{item.shop_name}}</span>
+                                        </div>
+                                </div>
+
+                            </router-link>
+
+                        </li>
+                    </ul>
+                </main>
 
             </div>
 
@@ -28,7 +75,19 @@ export default {
 
     methods:{
         goSearch (type){
-
+            let _this = this;
+            if (_this.keyword == '') return;
+            _this.$http.get('/search',{
+                params: {
+                    kw: _this.keyword,
+                    hot: type? type : ''
+                }
+            }).then((res) => {
+                console.log(res.data);
+                _this.mDatas = res.data;
+            }, (err) => {
+                console.log(err);
+            })
         }
     }
 
