bc1e9e12bec06851a09b4a73ea212de5eb2dd569
Jason Qu <fishinsea2014@gmail.com>
2018-07-21 19:08:46 +1200

    Create the footer. Still need to hide the footer of homepage and display the cart footer.

--- vue_online_shopping/src/components/CartFooter.vue
+++ vue_online_shopping/src/components/CartFooter.vue
@@ -1,11 +1,37 @@
 <template>
     <div class="car_fo">
-        
+        <footer class="cart_footer">
+		    <div class="all_check_box">
+		        <div class="check_box">
+
+		        </div>
+		        <span>Select All</span>
+		    </div>
+		    <div class="count_money_box">
+		        <div class="heji">
+		            <strong>Total:</strong> 
+		            <strong>$</strong>
+					<strong>{{cartTotal}}</strong>
+		        </div>
+		        <div class="total_money clearfix">
+		            <span>Total amount:</span>
+		            <i>$</i>
+		            <span>{{cartTotal}}</span>
+		            <span>Discount:</span>
+		            <i>$</i>
+		            <span>0.00</span>
+		        </div>>
+		            <a href="#" class="go_pay">
+		                <span>To Check Out</span>
+		            </a>
+		    </div>
+		</footer>
         
     </div>
 </template>
 
 <script>
+import {mapGetters}  from 'vuex';
 
 export default {
     data() {
@@ -15,6 +41,7 @@ export default {
     },
     components: {
         
-    }
+    },
+    computed: mapGetters(['cartTotal']),
 }
 </script>
